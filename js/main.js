2e4e954ce16733b5923ddf199ef7df19cf8c935b
KirillVN <kirill137777@gmail.com>
2022-05-03 22:19:44 +0300

    images added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ menuBtn.addEventListener('click', showNav)
 
 // slider
 import {nextSlide, showSlide, currentSlide, images} from "./slider.js"
-const intervalID = setInterval(nextSlide, 5000);
+const intervalID = setInterval(nextSlide, 3000);
 
 // indicators slider 
 import {activeDot, dots, getDot} from "./indicators.js"
